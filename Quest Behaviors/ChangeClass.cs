563da4b592efda635f7bed77f8573d013fdd5698
Angles24 <59443578+Angles24@users.noreply.github.com>
2020-01-18 07:37:27 +1000

    .

--- Quest Behaviors/ChangeClass.cs
+++ Quest Behaviors/ChangeClass.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using Buddy.Coroutines;
 using Clio.XmlEngine;
 using ff14bot.Enums;
+using ff14bot.Helpers;
 using ff14bot.Managers;
 using ff14bot.NeoProfiles;
-using ff14bot.RemoteWindows;
-using GreyMagic;
 using TreeSharp;
 
 namespace ff14bot
@@ -22,11 +20,10 @@ namespace ff14bot
         public override bool IsDone => _isDone;
 
         public override bool HighPriority => true;
-        
-        [XmlAttribute("Job")]
-        public string job { get; set; }
 
-        
+        [XmlAttribute("Job")] public string job { get; set; }
+
+
         protected override void OnStart()
         {
         }
@@ -43,14 +40,46 @@ namespace ff14bot
         private async Task<bool> ChangeJob()
         {
             var gearSets = GearsetManager.GearSets.Where(i => i.InUse);
-
+            Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Started");
             ClassJobType newjob;
-            var foundJob = ClassJobType.TryParse(job.Trim(), true, out newjob);
-
+            var foundJob = Enum.TryParse(job.Trim(), true, out newjob);
+            Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Found job: {foundJob} Job:{newjob}");
             if (foundJob && gearSets.Any(gs => gs.Class == newjob))
             {
+                Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Found GearSet");
                 gearSets.First(gs => gs.Class == newjob).Activate();
                 await Coroutine.Sleep(1000);
+            }
+            
+            else if (foundJob)
+            {
+                job = job.Trim() + ("s_Primary_Tool");
+
+                ItemUiCategory category;
+                var categoryFound = Enum.TryParse(job, true, out category);
+                
+                if (categoryFound)
+                {
+                    Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Found Item Category: {categoryFound} Category:{category}");
+                    var item = InventoryManager.FilledInventoryAndArmory.Where(i => i.Item.EquipmentCatagory == category).OrderByDescending(i=> i.Item.ItemLevel).FirstOrDefault();
+                    BagSlot EquipSlot = InventoryManager.GetBagByInventoryBagId(InventoryBagId.EquippedItems)[EquipmentSlot.MainHand];
+                    
+                    Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Found Item {item}");
+                    if (item != null)
+                    {
+                        item.Move(EquipSlot);
+                    }
+                    
+                    await Coroutine.Sleep(1000);
+                    
+                    ChatManager.SendChat("/gs save");
+                    
+                    await Coroutine.Sleep(1000);
+                }
+                else
+                {
+                    Logging.Write(Colors.Fuchsia, $"[ChangeJobTag] Couldn't find item category'");
+                }
             }
 
             _isDone = true;
